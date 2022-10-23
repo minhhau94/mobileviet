e99f02a20cc2b34c00aca73b834a45b33325f60d
minhhau94 <minhhau.tran94@gmail.com>
2022-10-24 04:14:48 +0700

    add

---
+++ model/product.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(id, type, name, price, screen, backCamera, frontCamera, battery, image, describe) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.price = price
+        this.screen = screen;
+        this.backCamera = backCamera;
+        this.frontCamera = frontCamera;
+        this.battery = battery;
+        this.img = image;
+        this.describe = describe;        
+    };
+}
